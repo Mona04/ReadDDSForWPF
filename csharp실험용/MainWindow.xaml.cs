b8e687c422c50a9d23b39e656332cad89c81b728
Mona04 <rudals0450@naver.com>
2021-05-18 01:21:16 +0900

    05.18.2021

--- csharp실험용/MainWindow.xaml.cs
+++ csharp실험용/MainWindow.xaml.cs
@@ -17,32 +17,6 @@ using System.Runtime.InteropServices;
 
 namespace csharp실험용
 {
-    public class TexHelper
-    {
-        [DllImport("kernel32.dll", EntryPoint = "RtlMoveMemory")]
-        public static extern void CopyMemory(IntPtr Destination, IntPtr Source, uint Length);
-
-        public static BitmapSource BitMapFromNativePointer(IntPtr pData, int w, int h, int ch)
-        {
-            var format = PixelFormats.Default;
-
-            if (ch == 1) format = PixelFormats.Gray8; //grey scale image 0-255
-            if (ch == 3) format = PixelFormats.Bgr24; //RGB
-            if (ch == 4) format = PixelFormats.Bgr32; //RGB + alpha
-
-
-            WriteableBitmap wbm = new WriteableBitmap(w, h, 96, 96, format, null);
-            CopyMemory(wbm.BackBuffer, pData, (uint)(w * h * ch));
-
-            wbm.Lock();
-            wbm.AddDirtyRect(new Int32Rect(0, 0, wbm.PixelWidth, wbm.PixelHeight));
-            wbm.Unlock();
-
-            return wbm;
-        }
-    }
-
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
